cfdd9a8cf76040e28027ddda8ca8a5f0417c8015
Marshall Zhang <mxzhang@college.harvard.edu>
2016-03-31 20:28:11 -0400

    updated table, going through again

--- empirics/test_lamperti_ou/process_odyssey_res.R
+++ empirics/test_lamperti_ou/process_odyssey_res.R
@@ -1,20 +1,24 @@
 
-ids = c(0) + 1:5 * 9
+ids = c(4) + 1:99 * 6
 ests = matrix(NA, nrow = 100, ncol = 3)
 counter = 1
 for (id in ids) {
-  f = readLines(file(paste("/Users/marshall/Documents/senior/thesis/empirics/test_lamperti_ou/odyssey_res/dump/out_exact_", id, ".txt", sep = ""), open="r"))
+  f = readLines(file(paste("/Users/marshall/Documents/senior/thesis/empirics/thetas/out_exact_many.", id, sep = ""), open="r"))
   last.par = 0
   for (i in 1:length(f)) {
     if (length(grep("par1", f[i])) > 0) {
       last.par = i
     }
   }
-  print(floor(last.par/200))
+  if (floor(last.par/200) <4 ) {
+    next
+  }
   pars = as.numeric(na.omit(as.numeric(strsplit(f[last.par + 1], " ")[[1]])))
   ests[counter, ] = pars
   counter = counter + 1
 }
 
+print(counter)
 print(colMeans(ests, na.rm = T))
+ests[,1] = ests[,1]/ests[,2]
 print((colSds(ests, na.rm = T)))
